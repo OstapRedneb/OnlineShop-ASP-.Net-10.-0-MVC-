4ef0661a73efe50bf7a7b13cdc05e82087d08bc7
OstapRedneb <rodionzabirov888@gmail.com>
2026-06-01 22:36:02 +0500

    Добавил контроллер Calculator с методом Index, который вычисляет сумму двух Int32. Добавил 2 необязательных сегмента, отвечающие за сумму чисел

--- OnlineShop/Program.cs
+++ OnlineShop/Program.cs
@@ -18,7 +18,7 @@ namespace OnlineShop
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}")
+                pattern: "{controller=Home}/{action=Index}/{firstNumber?}/{secondNumber?}")
                 .WithStaticAssets();
 
             app.Run();
