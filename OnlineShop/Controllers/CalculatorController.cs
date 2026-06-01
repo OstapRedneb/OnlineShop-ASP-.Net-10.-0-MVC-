cf49493b447c5373b37d9515435a69708b82250c
OstapRedneb <rodionzabirov888@gmail.com>
2026-06-01 22:36:02 +0500

    Добавил контроллер Calculator с методом Index, который вычисляет сумму двух Int32. Добавил 2 необязательных сегмента, отвечающие за сумму чисел

---
+++ OnlineShop/Controllers/CalculatorController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShop.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public int Index(int firstNumber, int secondNumber) => firstNumber + secondNumber;
+    }
+}
