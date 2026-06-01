41f3a4ec2de81aeec7049d7c0f4cbdccf6d00be0
OstapRedneb <rodionzabirov888@gmail.com>
2026-06-01 22:16:03 +0500

    Добавил контроллер Start с методом Hello, который выводит ссобщение с приветствием, в зависимости от времени дня

---
+++ OnlineShop/Controllers/StartController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShop.Controllers
+{
+    public class StartController : Controller
+    {
+        public string Hello()
+        {
+            DateTime time = DateTime.Now;
+
+            if (time.Hour < 6 && time.Hour >= 0)
+                return "Доброй ночи";
+            if (time.Hour >= 6 && time.Hour < 12)
+                return "Доброе утро";
+            if (time.Hour >= 12 && time.Hour < 18)
+                return "Добрый день";
+            if (time.Hour >= 18)
+                return "Добрый вечер";
+
+            return "Ошибка определения времени";
+        }
+    }
+}
