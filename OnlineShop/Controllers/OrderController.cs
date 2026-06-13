0a06f29cc1f99521ec0a0e11b87f2e766d4d22cb
OstapRedneb <rodionzabirov888@gmail.com>
2026-06-14 00:19:38 +0500

    Исправил отображение цены в корзине. Добавил заглушку на оформление заказов

---
+++ OnlineShop/Controllers/OrderController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Models;
+using OnlineShop.Services.Interfaces;
+
+namespace OnlineShop.Controllers
+{
+    public class OrderController(ICartService cartService) : Controller
+    {
+        public IActionResult Index()
+        {
+            Cart? cart = cartService.GetById(Info.Info.CommonCartId);
+
+            if (cart is null)
+                return RedirectToAction("Index", "Cart");
+
+            return View(cart);
+        }
+        public IActionResult Pay() 
+        {
+            Cart? cart = cartService.GetById(Info.Info.CommonCartId);
+
+            if (cart is null)
+                return RedirectToAction("Index", "Cart");
+
+            //Clear
+            cart.Clear();
+            cartService.Update(cart);
+
+            return RedirectToAction("Successfull");
+        }
+        public IActionResult Successfull() 
+        {
+            return View();
+        }
+    }
+}
