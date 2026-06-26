b83b2c7e2db4e1ee47969dc2b9de67390abd0672
OstapRedneb <rodionzabirov888@gmail.com>
2026-06-26 09:13:41 +0500

    Перенёс header из layout в частичное представление. В header добавил span с подсчётом числа товаров в корзине через компонент представления

---
+++ OnlineShop/Views/Shared/Components/CartCounter/CartCounterViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Models;
+using OnlineShop.Services.Interfaces;
+
+namespace OnlineShop.Views.Shared.Components.CartCounter
+{
+    public class CartCounterViewComponent(ICartService cartService) : ViewComponent
+    {
+        public IViewComponentResult Invoke() 
+        {
+            Cart? cart = cartService.GetById(Info.Info.CommonCartId);
+
+            int answer = cart is null ? 0 : cart.Sum(position => position.Quantity);
+
+            return View("CartCounter", answer);
+        }
+    }
+}
