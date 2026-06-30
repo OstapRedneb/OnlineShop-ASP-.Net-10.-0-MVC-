e3605d07e25f46d7ce669283272678cd5e677451
OstapRedneb <rodionzabirov888@gmail.com>
2026-06-30 16:54:26 +0500

    Добавил представление логина и соответствующий контроллер-заглушку

---
+++ OnlineShop/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShop.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
