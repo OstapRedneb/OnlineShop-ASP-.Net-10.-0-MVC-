382e30ff4546e94e634a77465735c1f28f011c74
OstapRedneb <rodionzabirov888@gmail.com>
2026-07-02 15:20:26 +0500

    Добавил админпанель

---
+++ OnlineShop/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShop.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Order()
+        {
+            return View();
+        }
+        public IActionResult User()
+        {
+            return View();
+        }
+        public IActionResult Role()
+        {
+            return View();
+        }
+        public IActionResult Product()
+        {
+            return View();
+        }
+    }
+}
