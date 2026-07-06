0976a7b54e77a578de05e0ed98c31a6afca49d43
OstapRedneb <rodionzabirov888@gmail.com>
2026-07-06 23:29:25 +0500

    Добавил поиск

--- OnlineShop/Controllers/HomeController.cs
+++ OnlineShop/Controllers/HomeController.cs
@@ -8,9 +8,15 @@ namespace OnlineShop.Controllers
 {
     public class HomeController(IProductService productService, ICartService cartService, IFavoriteService favoriteService, IOrderListService orderListService) : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+        public IActionResult Index(string searchString = "")
         {
             List<Product> products = productService.GetAll();
+
+            if (!string.IsNullOrWhiteSpace(searchString)) 
+            {
+                products = products.OrderByDescending(product => CountSearch(product.Name, searchString, 3)).ToList();
+            }
 
             return View(products);
         }
@@ -39,6 +45,26 @@ namespace OnlineShop.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private int CountSearch(string name, string searchString, int splitDiapason = 3) 
+        {
+            if (string.IsNullOrWhiteSpace(searchString) || splitDiapason > name.Length || name.Length < searchString.Length)
+                return 0;
+
+            string concatSearch = string.Concat
+                (
+                    searchString.Split([' ', '\t', '\n', ',', '.', '/', '\\', ':', ';', '-'], 
+                    StringSplitOptions.RemoveEmptyEntries)
+                );
+
+            int counter = 0;
+            for (int i = 0; i < concatSearch.Length - splitDiapason; i++) 
+            {
+                if (name.Contains(string.Concat(concatSearch.Skip(i).Take(splitDiapason))))
+                    counter++;
+            }
+            return counter;
         }
     }
 }
